2439ed223314ddcf1603fb624ae5e0450697a1da
Mattia04 <mattia.ballico@gmail.com>
2024-04-30 23:01:38 +0200

    created the file

---
+++ Mod/Fits.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy
+
+
+class Fits:
+    def linear(x, y, xerr, yerr):
+        pass
+
+    def exp():
+        pass
+
+    def sin():
+        pass
+
+    def smorzato():
+        pass
+
+
+class inviluppi:
+    pass
